8e1541b546632804de8fe6a316698ad007cb94a4
HazemKechiche <hazem.Kechiche@esprit.tn>
2021-11-14 17:01:35 +0100

    First commit main.cpp

---
+++ logement.cpp
@@ -0,0 +1,6 @@
+#include "logement.h"
+
+Logement::Logement()
+{
+
+}
---
+++ logement.h
@@ -0,0 +1,11 @@
+#ifndef LOGEMENT_H
+#define LOGEMENT_H
+
+
+class Logement
+{
+public:
+    Logement();
+};
+
+#endif // LOGEMENT_H
